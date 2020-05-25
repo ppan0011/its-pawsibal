55032cce0a289dfab4b33cfb736a3c425054f53b
ppan0011 <ppan0011@student.monash.edu>
2020-05-25 22:00:06 +1000

    Changes in controller files

--- app/Http/Controllers/SpeciesCategoryController.php
+++ app/Http/Controllers/SpeciesCategoryController.php
@@ -7,82 +7,6 @@ use Illuminate\Http\Request;
 
 class SpeciesCategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\SpeciesCategory  $speciesCategory
-     * @return \Illuminate\Http\Response
-     */
-    public function show(SpeciesCategory $speciesCategory)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\SpeciesCategory  $speciesCategory
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(SpeciesCategory $speciesCategory)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\SpeciesCategory  $speciesCategory
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, SpeciesCategory $speciesCategory)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\SpeciesCategory  $speciesCategory
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(SpeciesCategory $speciesCategory)
-    {
-        //
-    }
-
     public function getRecordsByRegion()
     {
         $data = SpeciesCategory::where('region', 'VIC')->paginate(12);
@@ -95,4 +19,11 @@ class SpeciesCategoryController extends Controller
         $data = SpeciesCategory::where('suburbs', 'like', $suburb)->paginate(9);
         return response()->json($data);
     }
+
+    public function searchSuburb(Request $request)
+    {
+        $suburb = $request->suburb;
+        $data = SpeciesCategory::where('suburbs', 'like', $suburb."%")->distinct()->get(['suburbs']);
+        return response($data);
+    }    
 }
